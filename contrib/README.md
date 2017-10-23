c10f470c0f84943b1176b6f3c48e358eeacf729a
Teteros <Teteros@users.noreply.github.com>
2017-10-23 13:52:04 +0100

    Add Windows usage scripts and contrib dir
    Adds information about the new `contrib` folder of the repo
    as well as adding `easage-pack` and `easage-unpack` Windows explorer
    helper scripts.

---
+++ contrib/README.md
@@ -0,0 +1,24 @@
+# `contrib`
+
+Contributed examples of how users may use the `easage` library and tools.
+
+---
+
+## `easage-pack.cmd` and `easage-unpack.cmd`
+
+Scripts for drag-and-drop operations in Windows' File Explorer.
+Handles multiple selections of alpha-numeric filenames or folders.
+
+To use simply select some files or folders and drop them on top of the desired `.cmd` script.
+`easage-pack.cmd` will pack your files/folders into the `BIG` format while `easage-unpack.cmd` unpacks selected `BIG`'s.
+A prefix for `easage-pack.cmd` `BIG` output(s) can be configured for your particular mod.
+
+## License
+
+[MIT](LICENSE.md)
+
+## Contributing
+
+Any contribution you intentionally submit for inclusion in the work, as defined
+in the `LICENSE.md` file, shall be licensed as above, and are subject to the
+project's [CLA](https://gist.github.com/Phrohdoh/d402395a3d8c453e4399f7ae345c0d72).
---
+++ contrib/easage-pack.cmd
@@ -0,0 +1,52 @@
+@echo off
+rem Contributor: Teteros
+rem Non-alphanumeric filenames were untested and might cause problems
+
+title Drag'n'Drop a selection onto this script to pack into BIGs
+rem Usage: Drag'n'Drop files and folders onto easage-pack.cmd
+rem to pack into BIG format files
+rem easage.exe 'pack' archives recursively
+rem i.e sub-directories are also packed
+if not exist easage.exe goto FileMissing
+
+rem Use wmic command to get current date information in YYYYMMDD format
+for /f %%# in ('wMIC Path Win32_LocalTime Get /Format:value') do @for /f %%@ in ("%%#") do @set %%@
+set "curdate=%year%%month%%day%"
+
+rem A prefix can be set for the output big files
+rem e.g. You may remove the date but keep the bangs
+rem
+rem Common sage modding convention:
+rem one '!' for mod release, two '!!' for hotfix/patch
+
+set modname=MOD
+set modver=001
+set "prefix=!%curdate%_%modname%%modver%_"
+set exit_timeout=5
+
+rem Uncomment line below (remove 'rem') to show your prefix and exit
+rem echo %prefix% & pause & exit
+
+:NotPacked
+if (%1) EQU () goto Packed
+echo Packing: "%~nx1" to "%prefix%%~n1.big"
+easage pack --source "%~nx1" --output "%prefix%%~n1.big"
+shift
+goto NotPacked
+
+:Packed
+echo.
+echo Selected files/folders packed into BIGs or you have not selected anything.
+echo.
+echo Exiting in %exit_timeout% seconds...
+timeout %exit_timeout% & exit /b 0
+
+:FileMissing
+echo.
+echo Current Directory is %CD%
+echo You need the easage.exe tool from easage
+echo library in your current directory to use this script.
+echo.
+echo See https://github.com/Phrohdoh/easage
+echo Aborting.
+pause & exit /b 1
