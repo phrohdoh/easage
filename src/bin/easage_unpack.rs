7e7dacb9e15ca3836b8eb0d4276687f1f71346d9
Teteros <Teteros@users.noreply.github.com>
2017-10-23 17:59:52 +0100

    Correct file path names that weren't renamed

--- src/bin/easage_unpack.rs
+++ src/bin/easage_unpack.rs
@@ -12,7 +12,7 @@ const ARG_NAME_OUTPUT: &'static str = "output";
 pub fn get_command<'a, 'b>() -> App<'a, 'b> {
     SubCommand::with_name(COMMAND_NAME)
         .version("0.0.1")
-        .about("Extract the contents of a BIG archive")
+        .about("Unpack the contents of a BIG archive")
         .author("Taryn Hill <taryn@phrohdoh.com>")
         .arg(Arg::with_name(ARG_NAME_SOURCE)
                 .long(ARG_NAME_SOURCE)
