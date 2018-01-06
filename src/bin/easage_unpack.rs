57bff726ac256701fb46bcb3e56a92a296b32605
Taryn Hill <taryn@phrohdoh.com>
2018-01-05 20:10:29 -0800

    easage-unpack: Implement the ability to unpack individual entries
    This commit closes #23 on the GitHub issue tracker.
    ref: https://github.com/Phrohdoh/easage/issues/23

--- src/bin/easage_unpack.rs
+++ src/bin/easage_unpack.rs
@@ -1,7 +1,7 @@
 use ::std::fs::{self, OpenOptions};
 use ::std::io::Write;
 use ::std::path::PathBuf;
-use clap::{Arg, ArgMatches, App, SubCommand};
+use clap::{Arg, ArgMatches, ArgGroup, App, SubCommand};
 
 use ::lib::Archive;
 use ::{CliResult, CliError};
@@ -9,6 +9,8 @@ use ::{CliResult, CliError};
 pub const COMMAND_NAME: &'static str = "unpack";
 const ARG_NAME_SOURCE: &'static str = "source";
 const ARG_NAME_OUTPUT: &'static str = "output";
+const ARG_NAME_NAMES: &'static str = "names";
+const ARG_NAME_ALL: &'static str = "all";
 
 const VERSION: &'static str = "0.0.1";
 
@@ -20,15 +22,26 @@ pub fn get_command<'a, 'b>() -> App<'a, 'b> {
         .arg(Arg::with_name(ARG_NAME_SOURCE)
                 .long(ARG_NAME_SOURCE)
                 .value_name(ARG_NAME_SOURCE)
-                .takes_value(true)
                 .required(true)
-                .help("Path to the BIG to unpack."))
+                .help("path to the BIG archive to unpack files from"))
         .arg(Arg::with_name(ARG_NAME_OUTPUT)
                 .long(ARG_NAME_OUTPUT)
                 .value_name(ARG_NAME_OUTPUT)
                 .takes_value(true)
                 .required(true)
-                .help("Path to the directory that should contain the BIG archive's contents."))
+                .help("path to the directory to write files to"))
+        .arg(Arg::with_name(ARG_NAME_NAMES)
+                .long(ARG_NAME_NAMES)
+                .value_name(ARG_NAME_NAMES)
+                .multiple(true)
+                .help("one or more entry names to extract"))
+        .arg(Arg::with_name(ARG_NAME_ALL)
+                .long(ARG_NAME_ALL)
+                .conflicts_with(ARG_NAME_NAMES)
+                .help("unpack all entries"))
+        .group(ArgGroup::with_name("to-extract")
+                .args(&[ARG_NAME_NAMES, ARG_NAME_ALL])
+                .required(true))
 }
 
 pub fn run(args: &ArgMatches) -> CliResult<()> {
@@ -36,10 +49,25 @@ pub fn run(args: &ArgMatches) -> CliResult<()> {
     let output = args.value_of(ARG_NAME_OUTPUT).unwrap();
     let output = PathBuf::from(output);
 
+    let mut names: Option<Vec<_>> = None;
+    let should_unpack_all = args.is_present(ARG_NAME_ALL);
+
+    if !should_unpack_all {
+        names = Some(args.values_of(ARG_NAME_NAMES).unwrap().collect::<Vec<_>>());
+    }
+
     let mut archive = Archive::from_path(source)?;
     let table = archive.read_entry_metadata_table()?;
 
-    for (entry_name, _entry) in &table {
+    for (entry_name, _entry) in table.iter() {
+        if !should_unpack_all {
+            if let Some(ref names) = names.as_ref() {
+                if names.contains(&entry_name.as_str()) {
+                    continue;
+                }
+            }
+        }
+
         if let Some(data) = archive.get_bytes_via_table(&table, entry_name) {
             let output_file = {
                 let mut o = output.clone();
